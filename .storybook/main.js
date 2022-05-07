fd5a283404b7f62ab02c29645c867645685abf85
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-05-07 08:16:53 +0200

    Storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,20 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: ["../stories/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
+  features: {
+    previewCsfV3: true,
+    interactionsDebugger: true,
+  },
+  addons: [
+    {
+      name: "@storybook/addon-docs",
+      options: {configureJSX: true},
+    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
-};
+  },
+}
---
+++ .storybook/theme.js
@@ -0,0 +1,22 @@
+import {create} from "@storybook/theming"
+
+export default create({
+  base: "light",
+  // UI
+  appContentBg: "#fff",
+  appBorderColor: "#333",
+  colorPrimary: "#00a8ff",
+  // appBorderRadius: 0,
+  // appBorderWidth: 0,
+  appHeaderBg: "#fafafa",
+  textColor: "#666",
+  barBg: "#666",
+
+  // Typography
+  fontBase: '"Roboto", sans-serif',
+  fontCode: "monospace",
+
+  brandTitle: "Edvin-js components",
+  // brandUrl: "https://example.com",
+  // brandImage: 'https://place-hold.it/350x150',
+})
