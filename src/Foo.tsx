df93cf3ac46fdf7b68208dbcc1709c90c6282b88
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-05-07 14:51:41 +0200

    useTimer

--- src/Foo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Foo = ({on}: any) => {
-  return <div>{on ? "on" : "off"}</div>
-}
-
-export {Foo}
--- src/timer.tsx
+++ src/timer.tsx
@@ -1,3 +1,31 @@
-const foo = () => {}
+import {useRef, useState} from "react"
 
-export {foo}
+const useTimer = () => {
+  const [startTime, setStartTime] = useState<null | number>(null)
+  const [now, setNow] = useState<null | number>(null)
+  const intervalRef: React.MutableRefObject<ReturnType<typeof setInterval> | undefined> = useRef()
+  // ReturnType<typeof setInterval>
+  const start = () => {
+    setStartTime(Date.now())
+    setNow(Date.now())
+
+    // @ts-ignore
+    clearInterval(intervalRef.current)
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now())
+    }, 10)
+  }
+
+  const stop = () => {
+    // @ts-ignore
+    clearInterval(intervalRef.current)
+  }
+
+  let secondsPassed = 0
+  if (startTime !== null && now !== null) {
+    secondsPassed = Math.floor((now - startTime) / 1000)
+  }
+  return [secondsPassed, start, stop]
+}
+
+export {useTimer}
